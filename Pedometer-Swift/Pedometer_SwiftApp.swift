844b6956534b56ba0c37e9aedfd3551205577b30
gaimo-ch <gaimovski.dev@gmail.com>
2023-04-19 16:04:13 +0900

    Initial Commit

---
+++ Pedometer-Swift/Pedometer_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pedometer_SwiftApp.swift
+//  Pedometer-Swift
+//
+//  Created by gaimo on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct Pedometer_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
